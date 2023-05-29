56ed0d9ab2c6aa08bfc596af226b3809b86d1d7c
Rufet <rufo.huseynov.1989@gmail.com>
2023-05-29 22:30:29 +0400

    Initial commit

---
+++ android/src/com/rufat/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rufat.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rufat.game.SurvivorBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SurvivorBird(), config);
+	}
+}
